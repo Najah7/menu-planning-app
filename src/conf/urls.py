72343a4fa1c8468e6de0b30495898818f85d316a
Najah7 <najahmatsuo7@gmail.com>
2024-10-03 15:50:31 +0900

    ✨ [Add] public and private home pages

--- src/conf/urls.py
+++ src/conf/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path(
+        "", TemplateView.as_view(template_name="public/home.html"), name="public_home"
+    ),
     path("admin/", admin.site.urls),
     path("home/", include("home.urls", namespace="home")),
     path("accounts/", include("accounts.urls", namespace="accounts")),
